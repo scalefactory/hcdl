7e69b7062d7bf81d8885aa42dda879f6e257d36d
David O'Rourke <david.orourke@gmail.com>
2020-06-09 22:36:50 +0100

    Add embedded_gpg_key feature

--- src/signature.rs
+++ src/signature.rs
@@ -10,7 +10,20 @@ use gpgrv::Keyring;
 use std::io::BufReader;
 use super::shasums::Shasums;
 
+#[cfg(not(feature = "embedded_gpg_key"))]
+use anyhow::anyhow;
+
+#[cfg(not(feature = "embedded_gpg_key"))]
+use std::fs::File;
+
+#[cfg(not(feature = "embedded_gpg_key"))]
+use std::io::prelude::*;
+
+#[cfg(feature = "embedded_gpg_key")]
 const HASHICORP_GPG_KEY: &'static str = include_str!("../gpg/hashicorp.asc");
+
+#[cfg(not(feature = "embedded_gpg_key"))]
+const HASHICORP_GPG_KEY_FILENAME: &'static str = "hashicorp.asc";
 
 pub struct Signature {
     signature: Bytes,
@@ -25,7 +38,8 @@ impl Signature {
 
     pub fn check(&self, shasums: &Shasums) -> Result<()> {
         let mut keyring = Keyring::new();
-        let gpg_key = BufReader::new(HASHICORP_GPG_KEY.as_bytes());
+        let gpg_key     = get_gpg_key()?;
+        let gpg_key     = BufReader::new(gpg_key.as_bytes());
 
         // compat handles error returned by failure crate
         match keyring.append_keys_from_armoured(gpg_key) {
@@ -46,4 +60,48 @@ impl Signature {
 
         Ok(())
     }
+}
+
+#[cfg(feature = "embedded_gpg_key")]
+fn get_gpg_key() -> Result<String> {
+    Ok(HASHICORP_GPG_KEY.to_owned())
+}
+
+#[cfg(not(feature = "embedded_gpg_key"))]
+fn get_gpg_key() -> Result<String> {
+    let mut path = match dirs::data_dir() {
+        Some(dir) => Ok(dir),
+        None      => Err(anyhow!("Couldn't find shared data directory")),
+    }?;
+
+    // Ensure that the data dir exists
+    if !path.exists() || !path.is_dir() {
+        let msg = anyhow!(
+            "Data directory {} does not exist or is not a directory",
+            path.display(),
+        );
+
+        return Err(msg);
+    }
+
+    path = path.join(env!("CARGO_PKG_NAME"));
+    path = path.join(HASHICORP_GPG_KEY_FILENAME);
+
+    // Ensure that the GPG key exists
+    if !path.exists() || !path.is_file() {
+        let msg = anyhow!(
+            "GPG key file {} does not exist or is not a file",
+            path.display()
+        );
+
+        return Err(msg);
+    }
+
+    let file         = File::open(&path)?;
+    let mut reader   = BufReader::new(file);
+    let mut contents = String::new();
+
+    reader.read_to_string(&mut contents)?;
+
+    Ok(contents)
 }
