b105dd1cbce524fd8f2f9d489697526f58607b56
David O'Rourke <david.orourke@gmail.com>
2020-07-01 21:01:25 +0100

    messages: Fix formatting

--- src/messages.rs
+++ src/messages.rs
@@ -26,27 +26,42 @@ impl Messages {
     }
 
     pub fn checksum_bad(&self, filename: &str) {
-        let msg = format!("SHA256 of {filename} did not match.", filename=filename);
+        let msg = format!(
+            "SHA256 of {filename} did not match.",
+            filename = filename,
+        );
+
         self.stderr(&msg);
     }
 
     pub fn checksum_ok(&self, filename: &str) {
-        let msg = format!("SHA256 of {filename} OK.", filename=filename);
+        let msg = format!("SHA256 of {filename} OK.", filename = filename);
+
         self.stdout(&msg);
     }
 
     pub fn downloading(&self, filename: &str) {
-        let msg = format!("Downloading {filename}...", filename=filename);
+        let msg = format!("Downloading {filename}...", filename = filename);
+
         self.stdout(&msg);
     }
 
     pub fn find_build_failed(&self, os: &str, arch: &str) {
-        let msg = format!("Could not find build for {os}-{arch}", os=os, arch=arch);
+        let msg = format!(
+            "Could not find build for {os}-{arch}",
+            os = os,
+            arch = arch,
+        );
+
         self.stderr(&msg);
     }
 
     pub fn installation_failed(&self, error: &Error) {
-        let msg = format!("Installation failed with error: {error}", error=error);
+        let msg = format!(
+            "Installation failed with error: {error}",
+            error = error,
+        );
+
         self.stderr(&msg);
     }
 
@@ -57,27 +72,32 @@ impl Messages {
     pub fn keep_zipfile(&self, filename: &str) {
         let msg = format!(
             "Keeping zipfile {filename} in current directory.",
-            filename=filename,
+            filename = filename,
         );
 
         self.stdout(&msg);
     }
 
     pub fn latest_version(&self, latest: &str) {
-        let msg = format!("Latest version: {latest}", latest=latest);
+        let msg = format!("Latest version: {latest}", latest = latest);
+
         self.stdout(&msg);
     }
 
     pub fn list_products(&self, products: &[&str]) {
-        let msg = format!("Products: {products}", products=products.join(", "));
+        let msg = format!(
+            "Products: {products}",
+            products = products.join(", "),
+        );
+
         self.stdout(&msg);
     }
 
     pub fn os_mismatch(&self, os: &str, requested: &str) {
         let msg = format!(
             "Product downloaded for different OS, {os} != {requested}",
-            os=os,
-            requested=requested,
+            os = os,
+            requested = requested,
         );
 
         self.stdout(&msg);
@@ -86,28 +106,37 @@ impl Messages {
     pub fn product_install(&self, product: &str, zipfile: &str, dest: &PathBuf) {
         let msg = format!(
             "Unzipping '{product}' from '{zipfile}' to '{dest}'",
-            product=product,
-            zipfile=zipfile,
-            dest=dest.display(),
+            product = product,
+            zipfile = zipfile,
+            dest = dest.display(),
         );
 
         self.stdout(&msg);
     }
 
     pub fn signature_verification_failed(&self, error: &Error) {
-        let msg = format!("Verification failed, error: {error}", error=error);
+        let msg = format!(
+            "Verification failed, error: {error}",
+            error = error,
+        );
+
         self.stderr(&msg);
     }
 
     pub fn signature_verification_success(&self, signature: &str) {
-        let msg = format!("Verified against {signature}.", signature=signature);
+        let msg = format!(
+            "Verified against {signature}.",
+            signature = signature,
+        );
+
         self.stdout(&msg);
     }
 
     pub fn skipped_install(&self, filename: &str) {
         let msg = format!(
-            "Skipping install and keeping zipfile '{filename}' in current directory.",
-            filename=filename,
+            "Skipping install and keeping zipfile '{filename}' in current \
+             directory.",
+            filename = filename,
         );
 
         self.stdout(&msg);
@@ -116,7 +145,7 @@ impl Messages {
     pub fn verifying_signature(&self, shasums: &str) {
         let msg = format!(
             "Downloading and verifying signature of {shasums}...",
-            shasums=shasums,
+            shasums = shasums,
         );
 
         self.stdout(&msg);
